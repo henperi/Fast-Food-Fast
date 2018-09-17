7265e1bd4b9b7e0eb72b475ed8c9995099e0c26e
henperi <pienry3g@gmail.com>
2018-09-17 22:08:59 +0100

    Setup the express server and install dependencies

---
+++ server.js
@@ -0,0 +1,71 @@
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import expressValidator from "express-validator";
+
+const app = express();
+
+// Some neccessary middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+// Express Validator Middleware
+app.use(
+  expressValidator({
+    errorFormatter: (param, msg, value) => {
+      const namespace = param.split(".");
+      const root = namespace.shift();
+      let formParam = root;
+
+      while (namespace.length) {
+        formParam += `[${namespace.shift()}]`;
+      }
+      return {
+        param: formParam,
+        msg,
+        value
+      };
+    }
+  })
+);
+
+app.get("/", (req, res) =>
+  res.status(200).send({
+    message:
+      "This is an API, not a website. Read more about the api endpoints below",
+    endpoints: [
+      {
+        type: "GET",
+        uri: "api/va/users/signin",
+        desired_parameters: ["email", "password"],
+        description:
+          "This endpoint uses the desired parameters to login or authenticate a visiting user"
+      }
+    ]
+  })
+);
+
+app.use("", (req, res) =>
+  res.status(404).json({
+    message:
+      "This endpoint does not exist. Read more about the api endpoints below",
+    endpoints: [
+      {
+        type: "GET",
+        uri: "api/va/users/signin",
+        desired_parameters: ["email", "password"],
+        description:
+          "This endpoint uses the desired parameters to login or authenticate a visiting user"
+      }
+    ]
+  })
+);
+
+// Define The Port and Host
+const PORT = process.env.PORT || 5000;
+
+// Start the node server
+app.listen(PORT, () => {
+  console.log(`Server Started Successfully On PORT: ${PORT}`);
+});
