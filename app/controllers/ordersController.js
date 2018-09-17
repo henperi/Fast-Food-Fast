0e0937e932931c7a9aa7e185451e1512349361e6
henperi <pienry3g@gmail.com>
2018-09-17 23:12:37 +0100

    Import Food.model in ordersController
    Import Food.model in ordersController inorder to use one of the methods (Food.findOne) available in Food's model class

--- app/controllers/ordersController.js
+++ app/controllers/ordersController.js
@@ -1,6 +1,6 @@
 // import randomId from 'uuid';
 import Order from '../models/Order.model';
-// import Food from '../models/Food.model';
+import Food from '../models/Food.model';
 
 const ordersController = {
   /**
@@ -89,65 +89,65 @@ const ordersController = {
    * @param {coverImg} coverImg is required
    * @returns {object} the created Order object
    */
-  //   makeAnOrder(req, res) {
-  //     req.checkBody('foodItems', 'Food Item(s) are required').notEmpty();
+  makeAnOrder(req, res) {
+    req.checkBody('foodItems', 'Food Item(s) are required').notEmpty();
 
-  //     const errors = req.validationErrors();
+    const errors = req.validationErrors();
 
-  //     if (errors) {
-  //       return res.status(400).json({ errors });
-  //     }
-  //     const userId = req.params.userId || 1; // authenticated userId
+    if (errors) {
+      return res.status(400).json({ errors });
+    }
+    const userId = req.params.userId || 1; // authenticated userId
 
-  //     const submittedFoodItems = req.body.foodItems;
-  //     // console.log('submittedFoodItems=', submittedFoodItems);
+    const submittedFoodItems = req.body.foodItems;
+    // console.log('submittedFoodItems=', submittedFoodItems);
 
-  //     const foodItemsOrdered = [];
-  //     let totalAmount = 0;
-  //     for (let i = 0; i < submittedFoodItems.length; i += 1) {
-  //       const [foodId] = [submittedFoodItems[i].foodId];
-  //       const [quantity] = [submittedFoodItems[i].quantity];
-  //       if (!foodId || !quantity) {
-  //         return res.status(400).json({
-  //           message: 'Order not created',
-  //           reasons:
-  //             'Submitted foodItem does not have a valid format. foodId param or quantity param is not defined',
-  //           description: `foodItems value must be an array containing object literals which have
-  //           foodId and quantity as parameters,
-  //           example: \n { foodItems: [{ foodId: 4801ac7c-4f19-4299-b709-aab25de4f088, quantity: 2 }] }.
-  //           visit /orders to see sample existing foodIds`,
-  //         });
-  //       }
+    const foodItemsOrdered = [];
+    let totalAmount = 0;
+    for (let i = 0; i < submittedFoodItems.length; i += 1) {
+      const [foodId] = [submittedFoodItems[i].foodId];
+      const [quantity] = [submittedFoodItems[i].quantity];
+      if (!foodId || !quantity) {
+        return res.status(400).json({
+          message: 'Order not created',
+          reasons:
+            'Submitted foodItem does not have a valid format. foodId param or quantity param is not defined',
+          description: `foodItems value must be an array containing object literals which have
+            foodId and quantity as parameters,
+            example: \n { foodItems: [{ foodId: 4801ac7c-4f19-4299-b709-aab25de4f088, quantity: 2 }] }.
+            visit /orders to see sample existing foodIds`,
+        });
+      }
 
-  //       const findFood = Food.findOne(submittedFoodItems[i].foodId);
-  //       // console.log('findFood:', findFood);
-  //       if (findFood) {
-  //         const item = {
-  //           foodId,
-  //           foodName: findFood.foodName,
-  //           coverImg: findFood.coverImg || `uploads/img/${foodItemsOrdered[i].foodId}`,
-  //           unitPrice: Number(findFood.unitPrice),
-  //           quantity: Number(submittedFoodItems[i].quantity),
-  //           total: Number(findFood.unitPrice * submittedFoodItems[i].quantity),
-  //           itemStatus: 'Processing',
-  //         };
-  //         foodItemsOrdered.push(item);
-  //         totalAmount += findFood.unitPrice * submittedFoodItems[i].quantity;
-  //         // console.log('totalAmount', totalAmount);
-  //       }
-  //     }
-  //     if (foodItemsOrdered.length > 0) {
-  //       const createdOrder = Order.createOrder(userId, foodItemsOrdered, totalAmount);
-  //       return res.status(201).json({
-  //         message: 'Order created',
-  //         createdOrder,
-  //       });
-  //     }
-  //     return res.status(201).json({
-  //       message: 'Order not created',
-  //       reasons: 'Submitted fooditem foodId(s) do not exist',
-  //     });
-  //   },
+      const findFood = Food.findOne(submittedFoodItems[i].foodId);
+      // console.log('findFood:', findFood);
+      if (findFood) {
+        const item = {
+          foodId,
+          foodName: findFood.foodName,
+          coverImg: findFood.coverImg || `uploads/img/${foodItemsOrdered[i].foodId}`,
+          unitPrice: Number(findFood.unitPrice),
+          quantity: Number(submittedFoodItems[i].quantity),
+          total: Number(findFood.unitPrice * submittedFoodItems[i].quantity),
+          itemStatus: 'Processing',
+        };
+        foodItemsOrdered.push(item);
+        totalAmount += findFood.unitPrice * submittedFoodItems[i].quantity;
+        // console.log('totalAmount', totalAmount);
+      }
+    }
+    if (foodItemsOrdered.length > 0) {
+      const createdOrder = Order.createOrder(userId, foodItemsOrdered, totalAmount);
+      return res.status(201).json({
+        message: 'Order created',
+        createdOrder,
+      });
+    }
+    return res.status(201).json({
+      message: 'Order not created',
+      reasons: 'Submitted fooditem foodId(s) do not exist',
+    });
+  },
 };
 
 export default ordersController;
