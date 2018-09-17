8d63cba57748bb65aaf5f78e3d83ad5bad3b294f
henperi <pienry3g@gmail.com>
2018-09-17 22:27:53 +0100

    Setup linting library and use airbnb style guide

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-import express from "express";
-import bodyParser from "body-parser";
-import morgan from "morgan";
-import expressValidator from "express-validator";
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import expressValidator from 'express-validator';
 
 const app = express();
 
 // Some neccessary middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 // Express Validator Middleware
 app.use(
   expressValidator({
     errorFormatter: (param, msg, value) => {
-      const namespace = param.split(".");
+      const namespace = param.split('.');
       const root = namespace.shift();
       let formParam = root;
 
@@ -24,43 +24,37 @@ app.use(
       return {
         param: formParam,
         msg,
-        value
+        value,
       };
-    }
-  })
+    },
+  }),
 );
 
-app.get("/", (req, res) =>
-  res.status(200).send({
-    message:
-      "This is an API, not a website. Read more about the api endpoints below",
-    endpoints: [
-      {
-        type: "GET",
-        uri: "api/va/users/signin",
-        desired_parameters: ["email", "password"],
-        description:
-          "This endpoint uses the desired parameters to login or authenticate a visiting user"
-      }
-    ]
-  })
-);
+app.get('/', (req, res) => res.status(200).send({
+  message: 'This is an API, not a website. Read more about the api endpoints below',
+  endpoints: [
+    {
+      type: 'GET',
+      uri: 'api/va/users/signin',
+      desired_parameters: ['email', 'password'],
+      description:
+          'This endpoint uses the desired parameters to login or authenticate a visiting user',
+    },
+  ],
+}));
 
-app.use("", (req, res) =>
-  res.status(404).json({
-    message:
-      "This endpoint does not exist. Read more about the api endpoints below",
-    endpoints: [
-      {
-        type: "GET",
-        uri: "api/va/users/signin",
-        desired_parameters: ["email", "password"],
-        description:
-          "This endpoint uses the desired parameters to login or authenticate a visiting user"
-      }
-    ]
-  })
-);
+app.use('', (req, res) => res.status(404).json({
+  message: 'This endpoint does not exist. Read more about the api endpoints below',
+  endpoints: [
+    {
+      type: 'GET',
+      uri: 'api/va/users/signin',
+      desired_parameters: ['email', 'password'],
+      description:
+          'This endpoint uses the desired parameters to login or authenticate a visiting user',
+    },
+  ],
+}));
 
 // Define The Port and Host
 const PORT = process.env.PORT || 5000;
