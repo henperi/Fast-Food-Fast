be045b0fcaad3f9d7f5805c5b741ccc092c911a4
henperi <pienry3g@gmail.com>
2018-09-26 11:05:43 +0100

    Add css and javascript to enable modal popups

--- UI/assets/js/responsive-nav.js
+++ UI/assets/js/responsive-nav.js
@@ -1,13 +1,37 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
 
 /**
- * 
+ * Toggle modals
  */
+var modal = document.querySelector(".modal");
+var trigger = document.querySelectorAll(".triggerModal");
+var closeButton = document.querySelectorAll(".close-button");
+
+toggleModal = () => {
+  modal.classList.toggle("show-modal");
+};
+
+windowOnClick = event => {
+  if (event.target === modal) {
+    toggleModal();
+  }
+};
+
+console.log(trigger[0].data);
+
+for (let i = 0; i < trigger.length; i++) {
+  trigger[i].addEventListener("click", toggleModal);
+}
+
+for (let i = 0; i < closeButton.length; i++) {
+  closeButton[i].addEventListener("click", toggleModal);
+}
+window.addEventListener("click", windowOnClick);
