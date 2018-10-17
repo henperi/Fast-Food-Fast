b38ffd8843931db80cc7df71be75af21fa055fc3
henperi <pienry3g@gmail.com>
2018-10-17 14:22:05 +0100

    Add fetch Profile and redirect functionality
    - Add a redirect controller that logs the user out and redirects him once the profile can not be fetched from the token provided.
    - Create a fetch method in the User.model.js class

---
+++ UI/src/redirectCtrl.js
@@ -0,0 +1,54 @@
+const main = document.querySelector('main');
+const container = document.querySelector('.container');
+const userToken = localStorage.getItem('userToken') || undefined;
+
+if (!userToken) {
+  localStorage.setItem('flashMsg', 'You need to login');
+  window.location.replace('../signin.html');
+}
+const localAPI = 'http://localhost:5000/api/v1';
+const remoteAPI = 'http://localhost:5000/api/v1';
+
+const fetchProfileUrl = `${localAPI}/users/my-profile`;
+fetch(fetchProfileUrl, {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json, text/plain, */*',
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json;charset=UTF-8',
+    'x-access-token': userToken,
+  },
+})
+  .then(res => res.json())
+  .then((data) => {
+    console.log('data', data);
+    // If no errors and signup is successfull
+    if (data.success) {
+      const { userId, role, fullname } = data.profile;
+
+      localStorage.setItem('userId', userId);
+      localStorage.setItem('fullname', fullname);
+      localStorage.setItem('role', role);
+      return;
+    }
+
+    const { errors } = data;
+    localStorage.clear();
+    localStorage.setItem('flashMsg', errors[0].msg);
+    window.location.replace('../signin.html');
+  })
+  .catch((error) => {
+    console.log('err', error);
+    container.classList.add('hide');
+    main.innerHTML = `
+    <div class="container text-center">
+      <section class="">
+        <h3 class="list-item text-red t-28">Poor network detected, refresh or try again in a moment</h3>
+        <div class="loader hide">
+          <i class="fa fa-spin fa-spinner"></i>
+        </div>
+        <button class="btn btn-primary reload">Reconnect</button>
+        </section>
+    </div>
+    `;
+  });
--- server/app/models/User.model.js
+++ server/app/models/User.model.js
@@ -40,6 +40,7 @@ class User {
         email: rows[0].email,
         mobile: rows[0].mobile,
         address: rows[0].address,
+        role: rows[0].role,
         createdAt: rows[0].created_at,
         updatedAt: rows[0].updated_at,
       };
@@ -62,6 +63,24 @@ class User {
     const queryText = 'SELECT * FROM users WHERE email = $1';
     try {
       const { rows } = await this.users.query(queryText, [email]);
+      return rows[0];
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        error_msg: 'An error occured while attempting to validate you, try again',
+      });
+    }
+  }
+
+  /**
+   * Find User By Email
+   * @param {email} email
+   * @returns {object} user object
+   */
+  async findById(req, res, userId) {
+    const queryText = 'SELECT user_id, fullname, email, mobile, address, role FROM users WHERE user_id = $1';
+    try {
+      const { rows } = await this.users.query(queryText, [userId]);
       return rows[0];
     } catch (err) {
       return res.status(500).json({
