3d5d6ba46f6cf072bc42c9561e66c8003a8ab8ea
henperi <pienry3g@gmail.com>
2018-10-26 13:34:16 +0100

    Create getOrdersCtrl.js and getItemsCtrl.js
    - Add method to consume/fetch the get all orders endpoint for Users
    - Add method to consume/fetch the get ordered Items endpoint for Users

---
+++ server/UI/src/getItemsCtrl.js
@@ -0,0 +1,49 @@
+const loader = document.querySelector('.loader');
+const responseArea = document.querySelector('.response-area');
+const tbody = document.querySelector('tbody');
+tbody.innerHTML = '';
+
+const getMyItmes = () => {
+  const orderId = window.location.href.split('?order_id=')[1];
+  const userId = localStorage.getItem('userId');
+  const getMyItmesUrl = `${localAPI}/users/${userId}/orders/${orderId}`;
+
+  fetch(getMyItmesUrl, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json;charset=UTF-8',
+      'x-access-token': userToken,
+    },
+  })
+    .then(res => res.json())
+    .then((data) => {
+      console.log(data);
+      // return;
+      if (data.success) {
+        for (let i = 0; i < data.items.length; i += 1) {
+          tbody.innerHTML += UserItmesComponent(data, i);
+        }
+        // tbody.innerHTML = '';
+      }
+      // tbody.innerHTML = ' ';
+    })
+    .catch((error) => {
+      console.log(error);
+      container.classList.add('hide');
+      main.innerHTML = renderPoorNetwork();
+    });
+};
+
+getMyItmes();
+
+const UserItmesComponent = (data, i) => `
+<tr>
+  <td>${data.items[i].food_name}</td>
+  <td>&#8358; ${data.items[i].unit_price}</td>
+  <td>${data.items[i].quantity}</td>
+  <td>&#8358; ${data.items[i].total}</td>
+  <td>${formatDate(data.items[i].created_at)}</td>
+  <td class="text-${data.items[i].itemstatus}">${data.items[i].itemstatus}</td>
+</tr>`;
